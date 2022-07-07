bbe1ffcbabb1a52e0d3f848d6958d3c8b77b2fa9
mario.razo <mario.razo@gonet.us>
2022-07-07 17:12:26 -0500

    Propuesta de DashBoard
      @Drawable
      -Se añaden varios SVGs
    
      @activity_main
      -Se crea un diseño completamente acorde a la tematica del sistema y responsivo. //No contiene funcionalidad
    
      Nota!
      Se crean archivos de recursos, pero hace falta crear un estandar

--- app/src/main/java/com/ops/opside/flows/sign_on/dashboard/MainActivity.kt
+++ app/src/main/java/com/ops/opside/flows/sign_on/dashboard/MainActivity.kt
@@ -1,12 +1,15 @@
-package com.ops.opside
+package com.ops.opside.flows.sign_on.dashboard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import com.ops.opside.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //asdasd
+        supportActionBar?.hide()
+
     }
 }
