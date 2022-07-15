933aa3e2bba64bc44465e0439bc6c95a280f3693
mario.razo <mario.v.r404@gmail.com>
2022-07-15 10:02:02 -0500

    15-07-2022 Base Dialog
    - Se crea un dialog que se pueda reutilizar en todo el proyecto

---
+++ app/src/main/java/com/ops/opside/common/dialogs/BaseDialog.kt
@@ -0,0 +1,53 @@
+package com.ops.opside.common.dialogs
+
+import android.app.Dialog
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import com.ops.opside.databinding.DialogBaseBinding
+
+class BaseDialog(
+    context: Context,
+    private val mTitle: String,
+    private val mDescription: String,
+    private val buttonYesText: String = "",
+    private val buttonNoText: String = "",
+    private val yesAction: () -> Unit = {},
+    private val noAction: () -> Unit = {}
+) : Dialog(context) {
+
+    private lateinit var binding: DialogBaseBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DialogBaseBinding.inflate(LayoutInflater.from(context))
+        setContentView(binding.root)
+
+        binding.apply {
+            tvTitle.text = mTitle
+            tvDescription.text = mDescription
+            btnYes.text = buttonYesText
+            btnNo.text = buttonNoText
+            btnNo.isVisible = buttonNoText.isNotEmpty()
+            btnYes.setOnClickListener {
+                yesAction.invoke()
+                dismiss()
+            }
+            btnNo.setOnClickListener {
+                noAction.invoke()
+                dismiss()
+            }
+
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val width = ViewGroup.LayoutParams.MATCH_PARENT
+        val height = ViewGroup.LayoutParams.WRAP_CONTENT
+        window?.setLayout(width, height)
+        window?.setBackgroundDrawableResource(android.R.color.transparent)
+    }
+}
