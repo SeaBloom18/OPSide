002915bf12972b2b75487874655d9e5b11fc309b
David Gonzalez <62410785+DavidGlezQ@users.noreply.github.com>
2022-07-28 21:08:48 -0500

    28-07-2022 - Fab Menu y timer
    Se agrega la opcion para elegir el tiempo de la jornada
    
    Se refactoriza los dos fab por un fab menu
    
    Se agregan recursos de animacion para fab menu
    
    Se elimina la opcion de panel de control del bottomNav junto con su respectivo modulo

--- app/src/main/java/com/ops/opside/flows/sign_on/dashboardModule/view/DashBoardFragment.kt
+++ app/src/main/java/com/ops/opside/flows/sign_on/dashboardModule/view/DashBoardFragment.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.fragment.app.Fragment
 import com.ops.opside.R
 import com.ops.opside.databinding.FragmentDashBoardBinding
@@ -18,10 +20,20 @@ class DashBoardFragment : Fragment() {
     private var mBinding: FragmentDashBoardBinding? = null
     private val binding get() = mBinding!!
 
+    private  val rotateOpen : Animation by lazy { AnimationUtils.loadAnimation(activity, R.anim.rotate_open_anim) }
+    private  val rotateClose : Animation by lazy { AnimationUtils.loadAnimation(activity, R.anim.rotate_close_anim) }
+    private  val fromBottom: Animation by lazy { AnimationUtils.loadAnimation(activity, R.anim.from_bottom_anim) }
+    private  val toBottom : Animation by lazy { AnimationUtils.loadAnimation(activity, R.anim.to_bottom_anim) }
+    private var closed = false
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?,
     ): View? {
         mBinding = FragmentDashBoardBinding.inflate(inflater, container, false)
+
+        binding.fabMenu.setOnClickListener {
+            OnAddButtonClick()
+        }
 
         binding.fabInitTaxCollection.setOnClickListener {
             val intent = Intent(activity, TaxCollectionActivity::class.java)
@@ -42,6 +54,36 @@ class DashBoardFragment : Fragment() {
         binding.imgSettings.setOnClickListener { startActivity(Intent(activity, ControlPanelActivity::class.java)) }
 
         return binding.root
+    }
+
+    private fun OnAddButtonClick() {
+        setVisibility(closed)
+        setAnimation(closed)
+        closed = !closed;
+    }
+
+    // A Function used to set the Animation effect
+    private fun setAnimation(closed:Boolean) {
+        if(!closed){
+            binding.fabTaxCollectionCrud.startAnimation(fromBottom)
+            binding.fabInitTaxCollection.startAnimation(fromBottom)
+            binding.fabMenu.startAnimation(rotateOpen)
+        }else{
+            binding.fabTaxCollectionCrud.startAnimation(toBottom)
+            binding.fabInitTaxCollection.startAnimation(toBottom)
+            binding.fabMenu.startAnimation(rotateClose)
+        }
+    }
+    // used to set visibility to VISIBLE / INVISIBLE
+    private fun setVisibility(closed:Boolean) {
+        if(!closed)
+        {
+            binding.fabTaxCollectionCrud.visibility = View.VISIBLE
+            binding.fabInitTaxCollection.visibility = View.VISIBLE
+        }else{
+            binding.fabTaxCollectionCrud.visibility = View.INVISIBLE
+            binding.fabInitTaxCollection.visibility = View.INVISIBLE
+        }
     }
 
     override fun onDestroy() {
