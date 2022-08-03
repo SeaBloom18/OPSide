f8ac371c2f76dbcd82a6d31b0e9896e6ff8bc874
David Gonzalez <62410785+DavidGlezQ@users.noreply.github.com>
2022-08-02 23:24:56 -0500

    02-08-2022 - custom toolbar
    Se agrega recurso de toolbar
    @toolbar_layout
    
    Se agregar primera configuracion para toolbar

--- app/src/main/java/com/ops/opside/flows/sign_on/incidentsModule/IncidentsFragment.kt
+++ app/src/main/java/com/ops/opside/flows/sign_on/incidentsModule/IncidentsFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import android.widget.Toolbar
 import com.ops.opside.R
 import com.ops.opside.databinding.FragmentIncidentsBinding
 
@@ -15,13 +17,43 @@ class IncidentsFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?, ): View? {
         // Inflate the layout for this fragment
         mBinding = FragmentIncidentsBinding.inflate(inflater, container, false)
+        setToolbar()
+
         return binding.root
+    }
+
+    private fun setToolbar(){
+        with(binding.toolbar.commonToolbar){
+            this.setTitleTextColor(R.color.primaryTextColor)
+            this.title = getString(R.string.bn_menu_incidents_opc5)
+            this.inflateMenu(R.menu.incidents_toolbar_menu)
+            this.setNavigationOnClickListener {
+            }
+
+            this.setOnMenuItemClickListener { menuItem ->
+                when(menuItem.itemId){
+                    R.id.create_incident -> {
+                        Toast.makeText(activity, "Create Incident", Toast.LENGTH_SHORT).show()
+                        true
+                    }
+
+                    R.id.see_incident -> {
+                        Toast.makeText(activity, "See Incident", Toast.LENGTH_SHORT).show()
+                        true
+                    }
+
+                    else -> false
+                }
+            }
+
+        }
     }
 
     override fun onDestroy() {
