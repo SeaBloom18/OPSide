597aefc875260c54bde636b4fa82a20bd6c6804b
David Gonzalez <62410785+DavidGlezQ@users.noreply.github.com>
2022-11-10 21:10:31 -0600

    10-11-2022 - Logica para actualizar hasAccess
    Logica para actualizar hasAccess

--- app/src/main/java/com/ops/opside/flows/sign_on/dashboardModule/adapter/ControlPanelAdapter.kt
+++ app/src/main/java/com/ops/opside/flows/sign_on/dashboardModule/adapter/ControlPanelAdapter.kt
@@ -4,8 +4,10 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.ops.opside.R
+import com.ops.opside.common.dialogs.BaseDialog
 import com.ops.opside.common.entities.share.CollectorSE
 import com.ops.opside.databinding.ItemControlPanelConcessionairePermissionBinding
 
@@ -14,6 +16,7 @@ RecyclerView.Adapter<ControlPanelAdapter.ViewHolder>(){
 
     private lateinit var context: Context
 
+    /** Adapter And ViewHolder Configuration**/
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         context = parent.context
         val view = LayoutInflater.from(context).inflate(R.layout.item_control_panel_concessionaire_permission, parent, false)
@@ -22,7 +25,16 @@ RecyclerView.Adapter<ControlPanelAdapter.ViewHolder>(){
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val collectors = collectorsList[position]
-        holder.binding.tvConcessionaireName.text = collectors.name
+        holder.apply {
+            binding.tvConcessionaireName.text = collectors.name
+            if (collectors.hasAccess) binding.switchHasAccess.isChecked = true
+            binding.switchHasAccess.setOnCheckedChangeListener { compoundButton, isChecked ->
+                if (isChecked) {
+                    changeHasAccess(collectors.name)
+                }
+                //binding.switchHasAccess.isChecked =
+            }
+        }
     }
 
     override fun getItemCount(): Int = collectorsList.size
@@ -35,5 +47,26 @@ RecyclerView.Adapter<ControlPanelAdapter.ViewHolder>(){
     /** Inner Class **/
     inner class ViewHolder(view: View): RecyclerView.ViewHolder(view){
         val binding = ItemControlPanelConcessionairePermissionBinding.bind(view)
+        
+        /** Other Methods**/
+        fun changeHasAccess(collectorName: String) {
+            val dialog = BaseDialog(
+                context,
+                imageResource = R.drawable.ic_ops_warning,
+                mTitle = context.getString(R.string.cp_alertdialog_title),
+                mDescription = context.getString(R.string.control_panel_alert_dialog_title, collectorName),
+                buttonYesText = context.getString(R.string.common_accept),
+                buttonNoText = context.getString(R.string.common_cancel),
+                yesAction = {
+                    //Update hasAccess to true
+                    binding.switchHasAccess.isChecked = true
+                },
+                noAction = {
+                    binding.switchHasAccess.isChecked = false
+                    //Update hasAccess to false
+                }
+            )
+            dialog.show()
+        }
     }
 }
