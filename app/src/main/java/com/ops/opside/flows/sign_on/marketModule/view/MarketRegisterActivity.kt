51e45d9770f2dae2196c9244694a116e202170f8
David Gonzalez <62410785+DavidGlezQ@users.noreply.github.com>
2023-01-08 12:42:45 -0600

    08-01-2023 - gradle update
    target sdk update

--- app/src/main/java/com/ops/opside/flows/sign_on/marketModule/view/MarketRegisterActivity.kt
+++ app/src/main/java/com/ops/opside/flows/sign_on/marketModule/view/MarketRegisterActivity.kt
@@ -85,7 +85,7 @@ class MarketRegisterActivity : BaseActivity(), OnMapReadyCallback {
                 val addresses: List<Address>
                 val geocoder = Geocoder(this@MarketRegisterActivity, Locale.getDefault())
 
-                addresses = geocoder.getFromLocation(latitudeMaps, longitudeMaps, 1)
+                addresses = geocoder.getFromLocation(latitudeMaps, longitudeMaps, 1) as List<Address>
 
                 addressSelected = addresses[0].getAddressLine(0)
 
